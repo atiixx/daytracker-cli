a0a3a473b8e4814857bc6c81833e32c6f3b47710
Jonas Hauenstein <jonas.hauenstein@outlook.de>
2025-01-08 20:03:23 +0100

    added main question feature

--- main.go
+++ main.go
@@ -1,11 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 )
 
+type Question struct {
+	ID           int      `json:"id"`
+	Title        string   `json:"title"`
+	Answers      []string `json:"answers"`
+	DefaultValue string   `json:"default_value"`
+	CSVName      string   `json:"csv"`
+}
+
+type ConfigData struct {
+	CSVFilepath string     `json:"csv_filepath"`
+	CSVFilename string     `json:"csv_filename"`
+	Questions   []Question `json:"questions"`
+}
+
 func main() {
-	fmt.Println("Hello, world.")
-	start_questions()
+	fmt.Println("Welcome to the day tracker. Select an answer from the possible choices or just type directly if no choices are presented.\nPress Enter if you want to choose the default choice displayed in the brackets.")
+	var csv_filepath, csv_filename string
+	var questions []Question
+	csv_filepath, csv_filename, questions = load_config()
+	var selected_answers map[string]string = start_questions(questions)
+	fmt.Println(selected_answers)
+	fmt.Println(csv_filename)
+	fmt.Println(csv_filepath)
+	// => load existing csv
+	// => check if field names changed: if yes prompt user if it wants to create new one
+	// => save answers to csv
 	SaveDataToCSV()
+}
+
+// Load config from config.json file
+// return: the csv filepath and name aswell as all the questions
+func load_config() (string, string, []Question) {
+
+	var err error
+	var configBytes []byte
+	configBytes, err = os.ReadFile("./config.json")
+	if err != nil {
+		fmt.Println("Could not load config file.")
+		log.Fatal(err)
+	}
+	var configData ConfigData
+	err = json.Unmarshal(configBytes, &configData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return configData.CSVFilename, configData.CSVFilepath, configData.Questions
 }
--- questions.go
+++ questions.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
 
-func start_questions() {
-	fmt.Println("start questions")
+	"github.com/fatih/color"
+)
+
+var printError = color.New(color.Bold, color.FgRed).PrintlnFunc()
+var printTitle = color.New(color.Bold, color.FgGreen).PrintfFunc()
+
+func start_questions(questions []Question) map[string]string {
+	answers := make(map[string]string, len(questions))
+	for _, q := range questions {
+	redo:
+		fmt.Println()
+		var answer string
+		var choices bool = len(q.Answers) > 0
+
+		if choices {
+			printTitle("%s: \n", q.Title)
+			for i, a := range q.Answers {
+				fmt.Printf("%d: %s\n", i+1, a)
+			}
+			fmt.Printf("Choose (1-%d): [Default: %s]\n", len(q.Answers), q.DefaultValue)
+			fmt.Scanf("%s", &answer)
+			if answer != "" {
+				number, err := strconv.Atoi(answer)
+				if err != nil {
+					printError("Error: Invalid input. Not a number")
+					goto redo
+				} else if number < 1 || number > len(q.Answers) {
+					printError("Error: Invalid input. Out of range")
+					goto redo
+				}
+				answers[q.CSVName] = q.Answers[number-1]
+			} else {
+				answers[q.CSVName] = q.DefaultValue
+			}
+
+		} else {
+			printTitle("%s: ", q.Title)
+			fmt.Printf("[Default: %s]\n", q.DefaultValue)
+			fmt.Scanf("%s", &answer)
+			if answer != "" {
+				answers[q.CSVName] = answer
+			} else {
+				answers[q.CSVName] = q.DefaultValue
+			}
+
+		}
+
+	}
+	return answers
 }
